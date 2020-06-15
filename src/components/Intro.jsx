8109f571083ca7bfa39ce23e9886d3fd9a98ef55
Luis Antonio Rodriguez Garcia <luis@luisrodriguezgarcia.com>
2020-06-15 15:36:15 -0500

    Modified some personal information

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import dev from '../assets/illustrations/dev.svg';
 
 const Intro = () => (
-  <>
+  <div className='Intro'>
     <div className='Details'>
       <h1>Hi There!</h1>
-      <h4>I’m John and I’m a JAMStack engineer!</h4>
-      <input type='button' href="#contact" name='Hire me' />
+      <h4>I’m Luis Antonio Rodríguez García I’m a Full Stack developer!</h4>
+      <input type='button' href="#contact" value='Contact me' />
     </div>
     <div className='Thumbnail'>
-      <img src={dev} alt="I’m John and I’m a JAMStack engineer!" />
+      <img src={dev} alt="I’m Luis Antonio Rodríguez García and I’m a Full Stack developer!" />
     </div>
-  </>
+  </div>
 );
 
 export default Intro;
