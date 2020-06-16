3bd98997d67cf217a50327bc3d86a73404043134
Luis Antonio Rodriguez Garcia <luis_rdz5@hotmail.com>
2020-06-15 21:34:55 -0500

    added a Wrapper to the project

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import dev from '../assets/illustrations/dev.svg';
+import Header from './Header';
+import '../styles/components/Intro.styl';
 
 const Intro = () => (
-  <div className='Intro'>
-    <div className='Details'>
-      <h1>Hi There!</h1>
-      <h4>I’m Luis Antonio Rodríguez García I’m a Full Stack developer!</h4>
-      <input type='button' href="#contact" value='Contact me' />
+  <div>
+    <Header />
+    <div className='Intro'>
+      <div className='Intro__details'>
+        <h1>Hi There!</h1>
+        <h4>I’m Luis Antonio Rodríguez García I’m a Full Stack developer!</h4>
+        <input type='button' href="#contact" value='Contact me' />
+      </div>
+      <div className='Intro__thumbnail'>
+        <img src={dev} alt="I’m Luis Antonio Rodríguez García and I’m a Full Stack developer!" />
+      </div>
     </div>
-    <div className='Thumbnail'>
-      <img src={dev} alt="I’m Luis Antonio Rodríguez García and I’m a Full Stack developer!" />
-    </div>
-  </div>
+  </div>  
 );
 
 export default Intro;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Header from './Header';
 import Footer from './Footer';
 import '../styles/components/Layout.styl';
 
 const Layout = ({ children }) => (
-  <div className="Main">
-    <Header />
-      {children}
-    <Footer />
+  <div className='Wrapper'>
+    <div className="Main">
+        {children}
+      <Footer />
+    </div>
   </div>
 );
 
